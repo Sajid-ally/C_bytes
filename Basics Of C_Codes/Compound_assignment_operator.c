1610fd8585adce75012fd9feb34d5b797a2425e5
Sajid <mohd.sajid120msd@gmail.com>
2025-08-11 01:42:11 +0530

    This code is an example for compound assignment operator

---
+++ Basics Of C_Codes/Compound_assignment_operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    a += 5;  // Equivalent to a = a + 5
+    b -= 3;  // Equivalent to b = b - 3
+
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", b);
+
+    return 0;
+}
+// This code demonstrates the use of compound assignment operators in C
