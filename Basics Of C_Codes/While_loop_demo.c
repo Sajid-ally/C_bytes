0a9850f873e34432286c45a88635b0d1191659c6
Sajid <mohd.sajid120msd@gmail.com>
2025-08-12 01:58:55 +0530

    This is first while loop program added

---
+++ Basics Of C_Codes/While_loop_demo.c
@@ -0,0 +1,10 @@
+//This is a program using while loop and print the name 5 times
+#include<stdio.h>
+int main(){
+    int i = 1;
+    while(i <= 5) {
+        printf(" Any Name\n");
+        i++;
+    }
+    return 0;
+}
