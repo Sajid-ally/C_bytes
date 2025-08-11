5a2095052990005d06d08b4292f47d1966379cc3
Sajid <mohd.sajid120msd@gmail.com>
2025-08-12 01:54:46 +0530

    Another example of if_else with OR operator added

---
+++ Basics Of C_Codes/Finding_vowels.c
@@ -0,0 +1,17 @@
+//This is an another example of C program to find vowels using if-else and OR operator
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("%c is a vowel.\n", ch);
+    } else {
+        printf("%c is not a vowel.\n", ch);
+    }
+
+    return 0;
+}
